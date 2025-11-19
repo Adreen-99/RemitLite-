56075ea3a12038a9559c7e594b2bc93feed52011
+ <+githinjiadreen27@gmail.com>
2025-11-20 00:11:34 +0300

    deployment

--- backend/app.py
+++ backend/app.py
@@ -626,4 +626,5 @@ for rule in app.url_map.iter_rules():
         print(f"   {rule.rule} -> {methods}")
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, port=5000)
